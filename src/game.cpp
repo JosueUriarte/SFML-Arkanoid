99f32459b8de63a5f09b146e45e155d08908444e
Josue Armando Uriarte Reyes <47550290+JosueUriarte@users.noreply.github.com>
2022-10-15 00:25:41 -0700

    New ball bounce prototype

--- src/game.cpp
+++ src/game.cpp
@@ -16,11 +16,11 @@ Game::Game(char* title)
 	// INIT THE PLAYER
 	this->player.setSize(sf::Vector2f(100.0f, 10.0f));
 	this->player.setPosition(250.0f, 480.0f);
-	this->player.setFillColor(sf::Color::Magenta);
+	this->player.setFillColor(sf::Color::Cyan);
 
 	// INIT THE BALL
 	this->ball.setRadius(5.0f);
-	this->ball.setOrigin(2.5f, 2.5f);
+	//this->ball.setOrigin(2.5f, 2.5f);
 	this->ball.setPosition(250.0f, 250.0f);
 	this->ball.setFillColor(sf::Color::White);
 
@@ -30,7 +30,7 @@ Game::Game(char* title)
 			{
 				this->blocks[this->iblocks].setSize(sf::Vector2f(60.0f, 20.0f));
 				this->blocks[this->iblocks].setPosition(61.0f * i + 5, 21.0f * j + 5);
-				this->blocks[this->iblocks].setFillColor(sf::Color::White);
+				this->blocks[this->iblocks].setFillColor(sf::Color::Green);
 				this->iblocks++;
 			}
 
@@ -39,6 +39,49 @@ Game::Game(char* title)
 bool isCollide(sf::CircleShape s1, sf::RectangleShape s2)
 {
 	return s1.getGlobalBounds().intersects(s2.getGlobalBounds());
+}
+
+bool newIntersection(sf::FloatRect circle, sf::FloatRect rectangle)
+{
+	// Rectangles with negative dimensions are allowed, so we must handle them correctly
+
+	// Compute the min and max of the circle on both axes
+	float r1MinX = std::min(circle.left, (circle.left + circle.width));
+	float r1MaxX = std::max(circle.left, (circle.left + circle.width));
+	float r1MinY = std::min(circle.top, (circle.top + circle.height));
+	float r1MaxY = std::max(circle.top, (circle.top + circle.height));
+
+	// Compute the min and max of the second rectangle on both axes
+	float r2MinX = std::min(rectangle.left, (rectangle.left + rectangle.width));
+	float r2MaxX = std::max(rectangle.left, (rectangle.left + rectangle.width));
+	float r2MinY = std::min(rectangle.top, (rectangle.top + rectangle.height));
+	float r2MaxY = std::max(rectangle.top, (rectangle.top + rectangle.height));
+
+	// Compute the intersection boundaries
+	float interLeft = std::max(r1MinX, r2MinX);
+	float interTop = std::max(r1MinY, r2MinY);
+	float interRight = std::min(r1MaxX, r2MaxX);
+	float interBottom = std::min(r1MaxY, r2MaxY);
+
+	// If the intersection is valid (positive non zero area), then there is an intersection
+	if ((interLeft < interRight) && (interTop < interBottom))
+	{
+		std::cout << "Left Side: " << interLeft << std::endl;
+		std::cout << "Right Side: " << interRight << std::endl;
+		std::cout << "Top Side: " << interTop << std::endl;
+		std::cout << "Bottom Side: " << interBottom << std::endl;
+		/*std::cout << "RECT MinX: " << r2MinX << std::endl;
+		std::cout << "RECT MaxX: " << r2MaxX << std::endl;
+		std::cout << "RECT MinY: " << r2MinY << std::endl;
+		std::cout << "RECT MaxY: " << r2MaxY << std::endl;*/
+		std::cout << "R-L Side: " << r2MaxX - interLeft << std::endl;
+		std::cout << '\n' << std::endl;
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
 
 Game::~Game()
@@ -70,7 +113,14 @@ void Game::update()
 	sf::FloatRect left_p(this->player.getPosition(), this->player.getSize()/2.0f);
 	if (this->ball.getGlobalBounds().intersects(left_p))
 	{
-		if (dx > 0) dx*=-1.0f;
+		float gbBall = this->ball.getGlobalBounds().left;
+		float gbPlayer = left_p.left + left_p.width;
+		std::cout << "GB COORDS L: " << gbPlayer - gbBall << std::endl;
+
+		dx = 1.0f;
+		dx += (gbPlayer - gbBall)/6;
+
+		if (dx > 0) dx *= -1.0f;
 		dy = -dy;
 		this->ball.setPosition(this->ball.getPosition().x, this->ball.getPosition().y - 3.0f);
 	}
@@ -79,6 +129,13 @@ void Game::update()
 	sf::FloatRect right_p(sf::Vector2f(this->player.getPosition().x + 50.0f, this->player.getPosition().y), this->player.getSize() / 2.0f);
 	if (this->ball.getGlobalBounds().intersects(right_p))
 	{
+		float gbBall = this->ball.getGlobalBounds().left;
+		float gbPlayer = this->player.getGlobalBounds().left + this->player.getGlobalBounds().width;
+		std::cout << "GB COORDS R: " << gbPlayer - gbBall << std::endl;
+
+		dx = 1.0f;
+		dx += (gbPlayer - gbBall) / 6;
+
 		if (dx < 0) dx*=-1.0f;
 		dy = -dy;
 		this->ball.setPosition(this->ball.getPosition().x, this->ball.getPosition().y - 3.0f);
